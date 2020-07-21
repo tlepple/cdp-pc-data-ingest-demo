230b019e1dbefd5aac2cebc5f5dc127d35e4043d
root <rtlepple@gmail.com>
2020-07-21 15:55:31 +0000

    initial commit

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#########################################################
+# utility functions
+#########################################################
+
+# change dir to install python3.6 from repo script
+cd ~
+
+git clone https://github.com/tlepple/py36.git
+
+cd ~/py36
+
+./setup.sh
+
+
